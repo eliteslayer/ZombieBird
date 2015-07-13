c9eae6d1fe9bbc7ad6ab888e1908ffe5d9dd6efc
Jacob Moellers <jacob.moellers@gmail.com>
2015-07-12 21:11:31 -0500

    Bird files pipes scroll. No Death no sound.

---
+++ core/src/com/kilobolt/gameobjects/Grass.java
@@ -0,0 +1,16 @@
+package com.kilobolt.gameobjects;
+
+/**
+ * Created by Jacob on 6/6/2015.
+ * Scrollable Grass
+ */
+public class Grass extends Scrollable {
+
+    // When Grass's constructor is invoked, invoke the super (Scrollable)
+    // constructor
+    public Grass(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+
+    }
+
+}
